fe91bcc5e555876c999c3197e43a326c889e3f09
Arjun <[empoliyan@gmail.com]>
2023-08-21 10:22:39 +0530

    Commit!

---
+++ Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WpfApp4.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string con;
+        public RepositoryBase()
+        {
+
+            con = @"Data Source=.;Initial Catalog=TestMVVM;Integrated Security=True";
+        }
+      protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(con);  
+        }
+    }
+}
